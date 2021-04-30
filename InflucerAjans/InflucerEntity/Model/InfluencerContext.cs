c5337ba7ab4436e9a2018451fcd1b4c64b485724
kadir1278 <kadirari@MAU-BIM5>
2021-04-30 13:38:44 +0300

    Veritabanı bağlantısı yapıldı ilk migration atıldı

---
+++ InflucerAjans/InflucerEntity/Model/InfluencerContext.cs
@@ -0,0 +1,26 @@
+﻿using InflucerEntity.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InflucerEntity.Model
+{
+    public class InfluencerContext:DbContext
+    {
+        public InfluencerContext(): base("Influencer")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+        public DbSet<About> Abouts{ get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+        }
+    }
+}
